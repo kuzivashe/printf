97c1fac7a1172f3bfa0c8cdc469032a7619c87a5
Kuzivashe <kuzibaye@gmail.com>
2022-10-18 20:08:46 -0700

    functions

--- functions.c
+++ functions.c
@@ -49,6 +49,30 @@ int print_string(va_list types, char buffer[],
 		if (precision >= 6)
 			str = "       ";
 	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision <= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
 	return (write(1, str, length));
 }
 
